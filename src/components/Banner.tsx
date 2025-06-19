4b066354975b8820ae86325bed3c9dac20b31ce4
백도현 <bbaekddo100@naver.com>
2025-06-19 12:12:49 +0900

    🚧 배너 컴포넌트 설정 - 영화 제목 추가 - 영화 설명 추가

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -7,8 +7,8 @@ import { useState } from "react";
 const Banner = () => {
   const [movie, setMovie] = useState<any>(null);
 
+  // 상영중인 영화 정보 불러오기
   const fetchMovies = async () => {
-    // 상영중인 영화 정보 불러오기
     const getNowPlayingResponse: AxiosResponse = await axiosInstance.get(
       movieRequests.fetchNowPlaying
     );
@@ -23,15 +23,43 @@ const Banner = () => {
     const movideMetadata: any = await axiosInstance.get(`movie/${movieId}`, {
       params: { append_to_response: "videos" },
     });
-    setMovie(movideMetadata);
-    console.log(movideMetadata);
+    setMovie(movideMetadata.data);
+  };
+
+  // 영화 설명 단축
+  const truncateDescription = (str: string, n: number) => {
+    return str?.length > n ? str.substring(0, n - 1) + "..." : str;
   };
 
   useEffect(() => {
     fetchMovies();
   }, []);
 
-  return <div>Banner</div>;
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundImage: `url('https://image.tmdb.org/t/p/original/${movie?.backdrop_path}')`,
+        backgroundPosition: "top center",
+        backgroundSize: "cover",
+        height: 500,
+      }}
+    >
+      <div className="banner__contents">
+        <h1 className="banner__title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+        <div className="banner__buttons">
+          {movie?.videos?.results[0]?.key && (
+            <button className="banner__button play">Play</button>
+          )}
+        </div>
+      </div>
+      <p className="banner__description">
+        {truncateDescription(movie?.overview, 100)}
+      </p>
+    </header>
+  );
 };
 
 export default Banner;
